428ea5356943c8362bb7818d70093026cd0fb438
Andrew <downleader@users.noreply.github.com>
2026-09-02 16:14:41 -0700

    Add: reverse-a-linked-list - submission-4

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode newHead = head;
+        if (head.next != null) {
+            newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+        }
+        return newHead;
+    }
+}
