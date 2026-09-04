b93376bbc4970f522e53efa1acee4e7580a0c685
Andrew <downleader@users.noreply.github.com>
2026-09-03 18:37:14 -0700

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head != null ? head.next : null;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode node = slow.next;
+        slow.next = null;
+
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        node = head;
+        while (node != null) {
+            ListNode leftNext = node.next;
+            ListNode rightNext = prev != null ? prev.next : null;
+
+            node.next = prev;
+            if (prev != null) {
+                prev.next = leftNext;
+            }
+
+            node = leftNext;
+            prev = rightNext;
+        }
+    }
+}
