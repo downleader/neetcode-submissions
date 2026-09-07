addcfa1c5dc78b18536122cf7d8c3a81cdc44af2
Andrew <downleader@users.noreply.github.com>
2026-09-07 11:24:03 -0700

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        int k = lists.length;
+
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+
+        PriorityQueue<HeapNode> heap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a.node.val, b.node.val)
+        );
+        for (int i = 0; i < k; i++) {
+            if (lists[i] != null) {
+                heap.add(new HeapNode(lists[i], i));
+            }
+        }
+
+        while (!heap.isEmpty()) {
+            HeapNode min = heap.remove();
+            node.next = min.node;
+            node = node.next;
+
+            ListNode next = min.node.next;
+            if (next != null) {
+                heap.add(new HeapNode(next, min.index));
+            }
+        }
+
+        return dummy.next;
+    }
+
+    private static class HeapNode {
+        final ListNode node;
+        final int index;
+
+        HeapNode(ListNode node, int index) {
+            this.node = node;
+            this.index = index;
+        }
+    }
+}
